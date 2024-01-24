9973dfcae98b6e9d5307551060455ad3460d103e
Yahya <YAHYA@DESKTOP-4QIIH5S>
2024-01-24 20:06:19 +0300

    Add project files.

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        T FindByCondition(Expression<Func<T, bool>> expression);
+        List<T> GetList();
+    }
+}
---
+++ EntityLayer/Concrete/Feature1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Feature1
+    {
+        public int Feature1Id { get; set; }
+        public string Post1Name { get; set; }
+        public string Post1Image { get; set; }
+        public string Post1Description { get; set; }
+        public bool Status { get; set; }
+    }
+}
