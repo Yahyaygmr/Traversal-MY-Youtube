4b9282730f9806e60af71b012e153cd8cbc0fc8d
Yahya <YAHYA@DESKTOP-4QIIH5S>
2024-01-29 14:23:21 +0300

    finished comments part

---
+++ TraversalCore/Controllers/CommentController.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCore.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly IServiceManager _serviceManager;
+
+        public CommentController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public PartialViewResult AddComment()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public IActionResult AddComment(Comment comment)
+        {
+            comment.CommentDate = DateTime.Now;
+            comment.CommentState = true;
+            _serviceManager.CommentService.TInsert(comment);
+            return RedirectToAction("Index","Destination");
+        }
+
+    }
+}
