e76253094adcdb3d7be23f1e16f09be63caacbb8
Yahya <YAHYA@DESKTOP-4QIIH5S>
2024-01-29 20:59:29 +0300

    End of the part 33

---
+++ TraversalCore/Areas/Members/Controllers/DestinationController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCore.Areas.Members.Controllers
+{
+    [Area("Members")]
+    [AllowAnonymous]
+    public class DestinationController : Controller
+    {
+        private readonly IServiceManager _serviceManager;
+
+        public DestinationController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _serviceManager.DestinationService.TGetList();
+            return View(values);
+        }
+    }
+}
