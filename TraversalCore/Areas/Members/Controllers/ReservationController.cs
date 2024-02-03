accdc36437ef96c0c0bfe095be32af90cc2779b0
Yahya <YAHYA@DESKTOP-4QIIH5S>
2024-02-03 15:29:40 +0300

    committed

---
+++ TraversalCore/Areas/Members/Controllers/ReservationController.cs
@@ -0,0 +1,30 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCore.Areas.Members.Controllers
+{
+    [Area("Members")]
+    [AllowAnonymous]
+    public class ReservationController : Controller
+    {
+        public IActionResult MyCurrentReservations()
+        {
+            return View();
+        }
+        public IActionResult MyOldtReservations()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult NewReservation()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult NewReservation(Reservation reservation)
+        {
+            return View();
+        }
+    }
+}
