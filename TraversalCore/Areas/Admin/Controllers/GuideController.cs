aa39f0217f7cc06f120087a86bd561bed2fcce29
Yahya <YAHYA@DESKTOP-4QIIH5S>
2024-02-07 22:54:11 +0300

    Created guide controller by admin

---
+++ TraversalCore/Areas/Admin/Controllers/GuideController.cs
@@ -0,0 +1,67 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class GuideController : Controller
+    {
+        private readonly IServiceManager _serviceManager;
+
+        public GuideController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        public IActionResult Index()
+        {
+            var guides = _serviceManager.GuideService.TGetList();
+            return View(guides);
+        }
+        [HttpGet]
+        public IActionResult CreateGuide()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateGuide(Guide guide)
+        {
+            _serviceManager.GuideService.TInsert(guide);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditGuide(int id)
+        {
+            var guide = _serviceManager.GuideService.TGetById(id);
+            return View(guide);
+        }
+        [HttpPost]
+        public IActionResult EditGuide(Guide guide)
+        {
+            _serviceManager.GuideService.TUpdate(guide);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteGuide(int id)
+        {
+            var guide = _serviceManager.GuideService.TGetById(id);
+            _serviceManager.GuideService.TDelete(guide);
+            return RedirectToAction("Index");
+        }
+        public IActionResult ChangeToTrue(int id)
+        {
+            var guide = _serviceManager.GuideService.TGetById(id);
+            guide.Status = true;
+            _serviceManager.GuideService.TUpdate(guide);
+            return RedirectToAction("Index");
+        }
+        public IActionResult ChangeToFalse(int id)
+        {
+            var guide = _serviceManager.GuideService.TGetById(id);
+            guide.Status = false;
+            _serviceManager.GuideService.TUpdate(guide);
+            return RedirectToAction("Index");
+        }
+    }
+
+}
