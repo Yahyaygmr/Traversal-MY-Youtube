28f9b8c0c3dd239b8adb6e253bf08330e2c033a7
Yahya <YAHYA@DESKTOP-4QIIH5S>
2024-02-10 15:50:24 +0300

    mail sender created

---
+++ TraversalCore/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,43 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using TraversalCore.Models;
+
+namespace TraversalCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Mail")]
+    public class MailController : Controller
+    {
+        [HttpGet]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("Index")]
+        public IActionResult Index(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+            MailboxAddress mailBoxAddresFrom = new MailboxAddress("Admin","yahyaygmr@gmail.com");
+            mimeMessage.From.Add(mailBoxAddresFrom);
+            MailboxAddress mailBoxAddresTo = new MailboxAddress("User", mailRequest.RecieverMail);
+            mimeMessage.To.Add(mailBoxAddresTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = mailRequest.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = mailRequest.Subject;
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Connect("smtp.gmail.com", 587, false);
+            smtpClient.Authenticate("yahyaygmr@gmail.com", "gmail sifresi");
+            smtpClient.Send(mimeMessage);
+            smtpClient.Disconnect(true);
+
+            return View();
+        }
+    }
+}
---
+++ TraversalCore/Models/MailRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace TraversalCore.Models
+{
+    public class MailRequest
+    {
+        public string Name { get; set; }
+        public string SenderMail { get; set; }
+        public string RecieverMail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
