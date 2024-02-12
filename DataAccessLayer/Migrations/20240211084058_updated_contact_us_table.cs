ee50a9adb03811006848ed7319cf207ef31c3645
Yahya <YAHYA@DESKTOP-4QIIH5S>
2024-02-12 15:00:29 +0300

    committed

---
+++ DataAccessLayer/Migrations/20240211084058_updated_contact_us_table.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class updated_contact_us_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "ContactUses");
+
+            migrationBuilder.RenameColumn(
+                name: "Surname",
+                table: "ContactUses",
+                newName: "NameSurname");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NameSurname",
+                table: "ContactUses",
+                newName: "Surname");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "ContactUses",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
