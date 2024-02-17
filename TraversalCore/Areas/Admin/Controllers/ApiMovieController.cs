d0b2f9371620da8c35f28d91a7af9c9f0cf86942
Yahya <YAHYA@DESKTOP-4QIIH5S>
2024-02-17 14:14:11 +0300

    added rapid api operations

---
+++ TraversalCore/Areas/Admin/Controllers/ApiMovieController.cs
@@ -0,0 +1,43 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.NetworkInformation;
+using System.Net.Http.Headers;
+using TraversalCore.Areas.Admin.Models;
+using Newtonsoft.Json;
+
+namespace TraversalCore.Areas.Admin.Controllers
+{
+    [AllowAnonymous]
+    [Area("Admin")]
+    [Route("Admin/ApiMovie")]
+    public class ApiMovieController : Controller
+    {
+        [Route("")]
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            List<ApiMovieViewModel> apiMovieList = new List<ApiMovieViewModel>();
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "b23f98432bmsh73554435a557e74p132c7fjsnf1ce1fb2aa23" },
+        { "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                apiMovieList = JsonConvert.DeserializeObject<List<ApiMovieViewModel>>(body);
+                return View(apiMovieList);
+            }
+            
+        }
+    }
+}
---
+++ TraversalCore/Areas/Admin/Models/BookingExchangeViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace TraversalCore.Areas.Admin.Models
+{
+    public class BookingExchangeViewModel
+    {
+            public bool status { get; set; }
+            public string message { get; set; }
+            public long timestamp { get; set; }
+            public Data data { get; set; }
+
+        public class Data
+        {
+            public Exchange_Rates[] exchange_rates { get; set; }
+            public string base_currency_date { get; set; }
+            public string base_currency { get; set; }
+        }
+
+        public class Exchange_Rates
+        {
+            public string currency { get; set; }
+            public string exchange_rate_buy { get; set; }
+        }
+
+    }
+}
